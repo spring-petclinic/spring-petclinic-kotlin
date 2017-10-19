d8f563094da17fb21aa4958d228098e6396d4a57
Antoine Rey <antoine.rey@gmail.com>
2017-10-19 18:40:02 +0200

    Remove unecessary Kotlin no-arg compiler plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,6 @@ plugins {
 
     id("org.jetbrains.kotlin.jvm") version kotlinVersion
     id("org.jetbrains.kotlin.plugin.spring") version kotlinVersion
-    id("org.jetbrains.kotlin.plugin.noarg") version kotlinVersion
     id("io.spring.dependency-management") version "1.0.3.RELEASE"
 }
 
