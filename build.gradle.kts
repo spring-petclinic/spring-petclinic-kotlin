b62e0bef1e6cfa05cda2ed72a66f48ec6b9a0191
sdeleuze <sdeleuze@pivotal.io>
2018-02-15 15:57:30 +0100

    Upgrade to Spring Boot 2.0.0.RC1

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 buildscript {
 
-    extra["kotlinVersion"] = "1.1.60"
-    extra["springBootVersion"] = "2.0.0.M7"
-    extra["jUnitVersion"] = "5.0.0"
+    extra["springBootVersion"] = "2.0.0.RC1"
     extra["boostrapVersion"] = "3.3.6"
     extra["jQueryVersion"] = "2.2.4"
     extra["jQueryUIVersion"] = "1.11.4"
@@ -30,7 +28,7 @@ plugins {
 
     id("org.jetbrains.kotlin.jvm") version kotlinVersion
     id("org.jetbrains.kotlin.plugin.spring") version kotlinVersion
-    id("io.spring.dependency-management") version "1.0.3.RELEASE"
+    id("io.spring.dependency-management") version "1.0.4.RELEASE"
 }
 
 apply {
@@ -69,8 +67,8 @@ dependencies {
     compile("org.springframework.boot:spring-boot-starter-webflux")
     compile("org.springframework.boot:spring-boot-starter-thymeleaf")
     compile("javax.cache:cache-api")
-    compile("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
-    compile("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
+    compile("org.jetbrains.kotlin:kotlin-stdlib")
+    compile("org.jetbrains.kotlin:kotlin-reflect")
     compile("org.webjars:webjars-locator")
     compile("org.webjars:jquery:$jQueryVersion")
     compile("org.webjars:jquery-ui:$jQueryUIVersion")
@@ -78,8 +76,8 @@ dependencies {
 
     testCompile("org.springframework.boot:spring-boot-starter-test")
     testCompile("org.glassfish:javax.el:$elVersion")
-    testCompile("org.junit.jupiter:junit-jupiter-api:$jUnitVersion")
-    testRuntime("org.junit.jupiter:junit-jupiter-engine:$jUnitVersion")
+    testCompile("org.junit.jupiter:junit-jupiter-api")
+    testRuntime("org.junit.jupiter:junit-jupiter-engine")
 
     runtime("org.hsqldb:hsqldb")
     runtime("mysql:mysql-connector-java")
