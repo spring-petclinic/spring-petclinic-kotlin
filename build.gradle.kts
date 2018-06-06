ab01dd081166431b5aaf91d8815be24511915255
Antoine Rey <antoine.rey@gmail.com>
2018-06-06 08:26:02 +0200

    #11 Use consistent gradle configurations

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,28 +30,28 @@ repositories {
 }
 
 dependencies {
-    compile("org.springframework.boot:spring-boot-starter-actuator")
-    compile("org.springframework.boot:spring-boot-starter-cache")
-    compile("org.springframework.boot:spring-boot-starter-data-jpa")
-    compile("org.springframework.boot:spring-boot-starter-web")
-    compile("org.springframework.boot:spring-boot-starter-thymeleaf")
-    compile("com.fasterxml.jackson.module:jackson-module-kotlin")
-    compile("javax.cache:cache-api")
-    compile("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    compile("org.jetbrains.kotlin:kotlin-reflect")
-    compile("org.webjars:webjars-locator-core")
-    compile("org.webjars:jquery:$jQueryVersion")
-    compile("org.webjars:jquery-ui:$jQueryUIVersion")
-    compile("org.webjars:bootstrap:$boostrapVersion")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-cache")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("javax.cache:cache-api")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.webjars:webjars-locator-core")
+    implementation("org.webjars:jquery:$jQueryVersion")
+    implementation("org.webjars:jquery-ui:$jQueryUIVersion")
+    implementation("org.webjars:bootstrap:$boostrapVersion")
 
-    testCompile("org.springframework.boot:spring-boot-starter-test") {
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(module = "junit")
     }
-    testCompile("org.springframework.boot:spring-boot-starter-webflux")
+    testImplementation("org.springframework.boot:spring-boot-starter-webflux")
     testImplementation("org.junit.jupiter:junit-jupiter-api")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 
-    runtime("org.hsqldb:hsqldb")
-    runtime("mysql:mysql-connector-java")
+    runtimeOnly("org.hsqldb:hsqldb")
+    runtimeOnly("mysql:mysql-connector-java")
 }
 
