058f6e1ee594a317277af3b3cd834318a21c9516
Antoine Rey <antoine.rey@gmail.com>
2018-06-19 19:59:35 +0200

    Upgrade to Spring Boot 2.0.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     val kotlinVersion = "1.2.30"
-    id("org.springframework.boot") version "2.0.2.RELEASE"
+    id("org.springframework.boot") version "2.0.3.RELEASE"
     id("org.jetbrains.kotlin.jvm") version kotlinVersion
     id("org.jetbrains.kotlin.plugin.spring") version kotlinVersion
     id("io.spring.dependency-management") version "1.0.4.RELEASE"
