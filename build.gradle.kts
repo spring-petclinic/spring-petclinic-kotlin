1c4864e96c645b7249d3c8f9de9abf7b33aea17b
Antoine Rey <antoine.rey@gmail.com>
2018-12-07 19:30:09 +0100

    Upgrade to Spring Boot 2.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     val kotlinVersion = "1.2.30"
-    id("org.springframework.boot") version "2.1.0.RELEASE"
+    id("org.springframework.boot") version "2.1.1.RELEASE"
     id("org.jetbrains.kotlin.jvm") version kotlinVersion
     id("org.jetbrains.kotlin.plugin.spring") version kotlinVersion
     id("io.spring.dependency-management") version "1.0.6.RELEASE"
