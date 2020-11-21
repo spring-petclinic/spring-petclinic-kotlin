429f88335e8d233ffc527bff009721a72611e5e7
Antoine Rey <antoine.rey@gmail.com>
2020-11-21 19:23:18 +0100

    Upgrade JIB plugin to 2.6.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
     val kotlinVersion = "1.4.10"
     id("org.springframework.boot") version "2.4.0"
     id("io.spring.dependency-management") version "1.0.10.RELEASE"
-    id("com.google.cloud.tools.jib") version "1.3.0"
+    id("com.google.cloud.tools.jib") version "2.6.0"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.spring") version kotlinVersion
 }
