5d31334180ba1ab89e7fcac4312c8beda37eea79
Antoine Rey <antoine.rey@free.fr>
2021-12-11 15:31:52 +0100

    Upgrade to Spring Boot 2.6.1 and Kotlin 1.6.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,14 +4,14 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 description = "Kotlin version of the Spring Petclinic application"
 group = "org.springframework.samples"
 // Align with Spring Version
-version = "2.6.0"
+version = "2.6.1"
 
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 plugins {
     val kotlinVersion = "1.6.0"
-    id("org.springframework.boot") version "2.6.0"
-    id("io.spring.dependency-management") version "1.0.10.RELEASE"
+    id("org.springframework.boot") version "2.6.1"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
     id("com.google.cloud.tools.jib") version "3.1.4"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.spring") version kotlinVersion
