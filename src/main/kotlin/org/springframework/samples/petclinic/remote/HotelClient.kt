05984214825c8e9e5fcbcf978be0cafbda6f4880
Test Name <test@unittest.com>
2024-04-30 13:51:24 +0200

    healthy project

--- src/main/kotlin/org/springframework/samples/petclinic/remote/HotelClient.kt
+++ src/main/kotlin/org/springframework/samples/petclinic/remote/HotelClient.kt
@@ -25,16 +25,16 @@ class HotelClient(
                 .body(RoomReservationResponse::class.java)
         }.call() ?: throw IllegalStateException("Missing body")
     }
-
-    fun requestRoomForPetWithoutCircuitBreaker(date: Date, petId: Int): RoomReservationResponse {
-        return client.post()
-            .uri("/room-reservation")
-            .contentType(MediaType.APPLICATION_JSON)
-            .body(RoomReservationRequest(date, petId))
-            .retrieve()
-            .body(RoomReservationResponse::class.java)
-            ?: throw IllegalStateException("Missing body")
-    }
+//
+//    fun requestRoomForPetWithoutCircuitBreaker(date: Date, petId: Int): RoomReservationResponse {
+//        return client.post()
+//            .uri("/room-reservation")
+//            .contentType(MediaType.APPLICATION_JSON)
+//            .body(RoomReservationRequest(date, petId))
+//            .retrieve()
+//            .body(RoomReservationResponse::class.java)
+//            ?: throw IllegalStateException("Missing body")
+//    }
 }
 
 data class RoomReservationRequest(val date: Date, val petId: Int)
